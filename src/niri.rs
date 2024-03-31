202036d6eb3c94994924fe711865155f754960e0
sodiboo <git@sodi.boo>
2024-03-31 04:59:31 +0200

    fix unused import

--- src/niri.rs
+++ src/niri.rs
@@ -30,8 +30,7 @@ use smithay::desktop::utils::{
     bbox_from_surface_tree, output_update, send_dmabuf_feedback_surface_tree,
     send_frames_surface_tree, surface_presentation_feedback_flags_from_states,
     surface_primary_scanout_output, take_presentation_feedback_surface_tree,
-    under_from_surface_tree, update_surface_primary_scanout_output, with_surfaces_surface_tree,
-    OutputPresentationFeedback,
+    under_from_surface_tree, update_surface_primary_scanout_output, OutputPresentationFeedback,
 };
 use smithay::desktop::{
     layer_map_for_output, LayerSurface, PopupGrab, PopupManager, PopupUngrabStrategy, Space,
